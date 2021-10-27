98de9c32926086c55c7cb63ed9faf2396c6a5e5c
Vijay Kumar <chunchuvijaykumar96@gmail.com>
2021-10-27 18:14:54 -0500

    updated users.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('mydata', { title: 'Vijay Kumar Chunchu ' });
+  res.render('mydata', { Text: 'I love dance' });
 });
 
 module.exports = router;
